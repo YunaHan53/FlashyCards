9a34bfb33a395273ff56fd3b1605a5a51cd3953f
YunaH <Yunahan53@gmail.com>
2026-02-11 16:53:40 -0500

    Initial commit with initial UI window created.

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy - the Interactive Flash Card app")
+window.config(bg=BACKGROUND_COLOR)
+
+
+window.mainloop()
