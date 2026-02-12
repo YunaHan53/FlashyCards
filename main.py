73184ebfa12c516e59c046e04c65278bf5e5b46e
YunaH <Yunahan53@gmail.com>
2026-02-12 15:56:06 -0500

    Updated next_card() function with better code

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ df = pd.read_excel(
     usecols='A:B',       # Only read specified columns
     nrows=100            # Only read first 100 rows
 )
-vocab_dictionary = df.to_dict()
+vocab_dictionary = df.to_dict(orient="records")
 
-def next_word():
-    word = random.choice(vocab_dictionary["Korean"])
-    canvas.itemconfig(word_text, text=f"{word}")
+def next_card():
+    current_card = random.choice(vocab_dictionary)
+    canvas.itemconfig(language_text, text="Korean")
+    canvas.itemconfig(word_text, text=current_card["Korean"])
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -28,20 +29,21 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=550, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Korean", font=("Ariel", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+language_text = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # card_back_img = PhotoImage(file="images/card_back.png")
 
 # Display buttons
 x_img = PhotoImage(file="images/wrong.png")
-x_button = Button(image=x_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
+x_button = Button(image=x_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_card)
 x_button.grid(row=1, column=0)
 
 check_img = PhotoImage(file="images/right.png")
-check_button = Button(image=check_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_word)
+check_button = Button(image=check_img, highlightthickness=0, bg=BACKGROUND_COLOR, command=next_card)
 check_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
